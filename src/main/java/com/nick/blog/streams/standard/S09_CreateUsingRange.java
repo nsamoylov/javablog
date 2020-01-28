d0bd0d4a837c25287f6221d429b09689c9632611
Nick Samoylov <nick.samoylov@ctl.io>
2020-01-27 19:27:31 -0700

    Add examples of range usage to create numeric streams

--- src/main/java/com/nick/blog/streams/standard/S09_CreateUsingRange.java
+++ src/main/java/com/nick/blog/streams/standard/S09_CreateUsingRange.java
@@ -1,25 +1,24 @@
 package com.nick.blog.streams.standard;
 
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
 public class S09_CreateUsingRange {
     public static void main(String... args) {
         System.out.print("1: ");
+        IntStream intStream1 = IntStream.range(3, 7);
+        intStream1.forEach(System.out::print);  //prints: 3456
 
         System.out.print("\n2: ");
+        IntStream intStream2 = IntStream.rangeClosed(3, 7);
+        intStream2.forEach(System.out::print);  //prints: 34567
 
         System.out.print("\n3: ");
+        LongStream longStream1 = LongStream.range(3, 7);
+        longStream1.forEach(System.out::print);  //prints: 3456
 
         System.out.print("\n4: ");
-
-        System.out.print("\n5: ");
-
-        System.out.print("\n6: ");
-
-        System.out.print("\n7: ");
-
-        System.out.print("\n8: ");
-
-        System.out.print("\n9: ");
-
-        System.out.print("\n10: ");
+        LongStream longStream2 = LongStream.rangeClosed(3, 7);
+        longStream2.forEach(System.out::print);  //prints: 34567
     }
 }
