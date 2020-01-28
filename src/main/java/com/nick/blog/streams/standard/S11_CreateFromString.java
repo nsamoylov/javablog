6878393be2052bfdedb60331f5c3daf4aeb18c07
Nick Samoylov <nick.samoylov@ctl.io>
2020-01-27 20:08:51 -0700

    Add examples of creating a stream from a String object

--- src/main/java/com/nick/blog/streams/standard/S11_CreateFromString.java
+++ src/main/java/com/nick/blog/streams/standard/S11_CreateFromString.java
@@ -1,25 +1,62 @@
 package com.nick.blog.streams.standard;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class S11_CreateFromString {
     public static void main(String... args) {
         System.out.print("1: ");
+        IntStream intStream = "abc".chars();
+        intStream.forEach(i -> System.out.print(i + " ")); //prints: 97 98 99
 
         System.out.print("\n2: ");
+        IntStream intStream2 = "abc".chars();
+        intStream2.mapToObj(c -> (char)c)
+                .forEach(i -> System.out.print(i + " "));  //prints: a b c
 
         System.out.print("\n3: ");
+        int[] chars = "abcDeF".chars()
+                .filter(c -> c < 97)  // avoids boxing into Character
+                .toArray();
+
+        Arrays.stream(chars)
+                .forEach(c -> System.out.print(c + " ")); //prints: 68 70
 
         System.out.print("\n4: ");
+        Arrays.stream(chars)
+                .forEach(c -> System.out.print((char)c + " ")); //prints: D F
 
         System.out.print("\n5: ");
+        System.out.print('a' > Character.MIN_SUPPLEMENTARY_CODE_POINT);      //prints: false
 
         System.out.print("\n6: ");
+        System.out.print(0x1F600 > Character.MIN_SUPPLEMENTARY_CODE_POINT); //prints: true
 
         System.out.print("\n7: ");
+        String supplString = new String(Character.toChars(0x1F600));
+        System.out.print(supplString);    //prints: 😀
 
         System.out.print("\n8: ");
+        //String supplString = new String(Character.toChars(0x1F600) );
+        supplString.chars().forEach(c -> System.out.println(c));  //prints: 55357 56832
 
         System.out.print("\n9: ");
+        supplString.chars().forEach(c -> System.out.println((char)c)); //prints: ? ?
 
         System.out.print("\n10: ");
-    }
+        supplString.chars().forEach(c -> System.out.println(Character.toChars(c))); //prints: ? ?
+
+        System.out.print("\n11: ");
+        supplString.codePoints().forEach(System.out::print);     //prints: 128512
+
+        System.out.print("\n12: ");
+        supplString.codePoints().forEach(c -> System.out.print((char)c));  //prints: 
+
+        System.out.print("\n13: ");
+        supplString.codePoints().forEach(c -> System.out.print(Character.toChars(c))); //prints: 😀
+
+        System.out.println("\n14: ");
+        Stream<String> linesStream = "Once\nupon\ra\r\ntime".lines();
+        linesStream.forEach(System.out::println);    }
 }
