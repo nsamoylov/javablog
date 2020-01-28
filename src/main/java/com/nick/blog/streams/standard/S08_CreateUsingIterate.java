a3edd688649dbdd0c8c58846cc47f5d3e468b728
Nick Samoylov <nick.samoylov@ctl.io>
2020-01-27 19:24:48 -0700

    Add examples of iterate usage

--- src/main/java/com/nick/blog/streams/standard/S08_CreateUsingIterate.java
+++ src/main/java/com/nick/blog/streams/standard/S08_CreateUsingIterate.java
@@ -1,25 +1,16 @@
 package com.nick.blog.streams.standard;
 
+import java.util.stream.Stream;
+
 public class S08_CreateUsingIterate {
     public static void main(String... args) {
         System.out.print("1: ");
+        Stream<Integer> stream1 = Stream.iterate(3, i -> i + 2);
+        stream1.limit(5).forEach(System.out::print);  //prints: 357911
 
         System.out.print("\n2: ");
+        Stream<Integer> stream2 = Stream.iterate(3, i -> i < 12, i -> i + 2);
+        stream2.forEach(System.out::print);            //prints: 357911
 
-        System.out.print("\n3: ");
-
-        System.out.print("\n4: ");
-
-        System.out.print("\n5: ");
-
-        System.out.print("\n6: ");
-
-        System.out.print("\n7: ");
-
-        System.out.print("\n8: ");
-
-        System.out.print("\n9: ");
-
-        System.out.print("\n10: ");
     }
 }
