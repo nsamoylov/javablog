0d2e05f5991f7f11f441cdb478640831b64b7e47
Nick Samoylov <nick.samoylov@ctl.io>
2020-04-28 15:01:13 -0600

    Add sorting example

--- src/main/java/com/nick/blog/streams/standard/intermediate/S16_sort.java
+++ src/main/java/com/nick/blog/streams/standard/intermediate/S16_sort.java
@@ -7,9 +7,15 @@ public class S16_sort {
     public static void main(String... args){
         System.out.print("1: ");
         Stream.of("2", "4", "3", "4", "2")
-                .sorted().forEach(System.out::print);  //prints: 22344
+                .sorted()
+                .forEach(System.out::print);  //prints: 22344
 
         System.out.print("\n2: ");
+        Stream.of("2", "4", "3", "4", "2")
+                .sorted(Comparator.comparingInt(s -> Integer.parseInt(s)))
+                .forEach(System.out::print);  //prints: 22344
+
+        System.out.print("\n3: ");
         Stream.of("2", "4", "3", "4", "2")
                 .sorted(Comparator.reverseOrder())
                 .forEach(System.out::print);           //prints: 44322
